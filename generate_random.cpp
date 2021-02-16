76b4f4332fff4980494b78ab929670e965f007b9
kairuihu <kairuihu2903@github.com>
2021-02-16 20:40:17 +0000

    i have completed questions 1-5

--- generate_random.cpp
+++ generate_random.cpp
@@ -30,26 +30,26 @@ int main()
     for (int i = 0; i < 10; i ++)
         cout << rand() << '\t';
     cout << "\nQ1: What is the output of rand(). Why? " << endl;
-    answer = "" ; // 1. Write your answer to Q1 in between " "
+    answer = "the output of rand() is ten random numbers between 0 to RAND_MAX, RAND_MAX is a constant interget that is already defined bythe cstdlib" ; // 1. Write your answer to Q1 in between " "
     cout << "Answer Q1: " << answer << endl;
 
     cout << endl;
     for (int i = 0; i < 10; i ++)
         cout << rand()%5 << '\t';
     cout << "\nQ2: What is the output of rand()%5. Why? " << endl;
-    answer = "" ; // 2. Write your answer to Q2 in between " "
+    answer = "% means modulo and the answer that will be provided will be the remainder of the division between two numbers, thus the output of rand()%5 will be the remainder of the divison between a random number and 5 and thus will be between 0-5" ; // 2. Write your answer to Q2 in between " "
     cout << "Answer Q2: " << answer << endl;
 
     cout << endl;
     for (int i = 0; i < 10; i ++)
         cout << rand()%5+3 << '\t';
     cout << "\nQ3: What is the output of rand()%5+3. Why?" << endl;
-    answer = "" ; // 3. Write your answer to Q3 in between " "
+    answer = "the output of rand()%5+3 is the remainder of the division between a random number and 5, after this the remainder is added with 3 thus providing a number between 3-7" ; // 3. Write your answer to Q3 in between " "
     cout << "Answer Q3: " << answer << endl;
 
     // Comment the line srand(time(NULL)); compile and run the program twice
     cout << "\nQ4: What is the output of the program when srand(time(NULL)) is removed. Why" << endl;
-    answer = "" ; // 4. Write your answer to Q4 in between " "
+    answer = "srand(time(NULL)) assigns the current time to the seed which allows the seed to progress, without the srand(time(NULL)) the seed does not progress thus the result is the randomly generated numbers will stay the same because there is no change to the state of the machine" ; // 4. Write your answer to Q4 in between " "
     cout << "Answer Q4: " << answer << endl;
 
     // 5. Change the rand() expression below to generate a random number 
@@ -58,7 +58,7 @@ int main()
     bool is_error = false; 
     for (int i = 0; i < 50; i ++)
     {
-        int dice_output = rand(); //change this expression so dice_output has a value between 1 and 6
+        int dice_output = rand()%6+1; //change this expression so dice_output has a value between 1 and 6
         if (dice_output < 1 || dice_output > 6)
         {
             cout << "Error: your code does not generate # between 1 and 6" << endl;
